b8004fe4657302e2a847754ff3d1b82265ce076e
Vr3n <vickyspatel@gmail.com>
2022-12-21 11:55:57 +0530

    Redux added

---
+++ slice/basketSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+export const basketSlice = createSlice({
+  name: "basket",
+  initialState,
+  reducers: {
+    addToBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {},
+  },
+});
+
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
+
+// Selectors - This is how er pull information from the global store slice.
+export const selectItems = (state: { basket: { items: [] } }) =>
+  state.basket.items;
+
+export default basketSlice.reducer;
